1e2c56c2da864c1dc079e7d83db2bf4df0b56374
Ming-H <1518246548@qq.com>
2025-02-10 21:32:55 +0800

    部署不生效

--- next.config.js
+++ next.config.js
@@ -7,6 +7,9 @@ const nextConfig = {
         deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
         formats: ['image/avif', 'image/webp'],
     },
+    output: 'standalone',
+    generateEtags: false,
+    poweredByHeader: false,
 }
 
 module.exports = withMDX(nextConfig) 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+export const revalidate = 0
+
 import Image from "next/image";
 import Link from "next/link";
 import { ArrowRight } from "lucide-react";
